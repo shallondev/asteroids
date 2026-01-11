8ee336ed2d1cb5d2233548b648de4974985545c4
shallondev <shallon.frances.pawlickpotts@gmail.com>
2026-01-11 15:38:31 -0500

    B: Adding constants

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,8 +1,10 @@
-import pygame # type: ignore
+import pygame  # type: ignore
+import constants
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
