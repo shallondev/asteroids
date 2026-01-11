cb4b8de0176fd7adb4d82828d573ebace816dac9
shallondev <shallon.frances.pawlickpotts@gmail.com>
2026-01-11 16:12:48 -0500

    E: Add FPS

--- asteroids/main.py
+++ asteroids/main.py
@@ -6,6 +6,9 @@ def main():
     pg.init()
     screen = pg.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     
+    clock = pg.time.Clock()
+    dt = 0
+
     while True:
 
         for event in pg.event.get():
@@ -15,10 +18,10 @@ def main():
         log_state()
         screen.fill("black")
         pg.display.flip()
-    
-    # print(f"Starting Asteroids with pygame version: {pg.version.ver}")
-    # print(f"Screen width: {constants.SCREEN_WIDTH}")
-    # print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+        dt = clock.tick(60) / 1000
+        print(dt)
+
 
 if __name__ == "__main__":
     main()
