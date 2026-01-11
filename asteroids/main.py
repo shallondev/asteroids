3874f11cc241c713061646cf7f7b102a3e9437ee
shallondev <shallon.frances.pawlickpotts@gmail.com>
2026-01-11 16:00:04 -0500

    D: Display black screen

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,10 +1,24 @@
-import pygame  # type: ignore
+import pygame as pg # type: ignore
 import constants
+from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pg.init()
+    screen = pg.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    
+    while True:
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        log_state()
+        screen.fill("black")
+        pg.display.flip()
+    
+    # print(f"Starting Asteroids with pygame version: {pg.version.ver}")
+    # print(f"Screen width: {constants.SCREEN_WIDTH}")
+    # print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
